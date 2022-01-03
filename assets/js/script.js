bb317d7a1bc305a90a20b6024f234c06a7ce5e9c
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2022-01-02 23:35:17 -0300

    2022 01 02 - Set Script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-import * as PIXI from "https://cdn.skypack.dev/pixi.js";
-import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur";
-import SimplexNoise from "https://cdn.skypack.dev/simplex-noise";
-import hsl from "https://cdn.skypack.dev/hsl-to-hex";
-import debounce from "https://cdn.skypack.dev/debounce";
+import * as PIXI from 'pixi.js';
+import { KawaseBlurFilter } from '@pixi/filter-kawase-blur';
+import SimplexNoise from 'simplex-noise';
+import hsl from 'hsl-to-hex';
+import debounce from 'debounce';
 
 // return a random number within a range
 function random(min, max) {
